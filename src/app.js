4f95f65ad013a673a6a7908cc6537e6d8181f845
DavidDlp <david.delpozo@bootcamp-upgrade.com>
2021-11-13 20:07:06 +0100

    FEAT: before merge David

--- src/app.js
+++ src/app.js
@@ -22,21 +22,6 @@ app.use(express.urlencoded({ extended: true }));
 
 
 
-//ERRORS
- /* app.use((error,req,res,next)=>{
-    const exception = {
-        status: defaults(error.status, 500),
-        messege: defaults(error.messege,'unexpected error')
-    }
-    
-    if(process.env.NODE_ENV !== 'production'){
-        exception['callstack'] = error.stack;
-    }
-    
-    console.error(exception);
-    res.status(exception.status).json(exception)
-}); */
-
 //CONECT DB
 const PORT = 3000;
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
