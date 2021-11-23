d3a8ab437d03655e6c90f9e8979c3363e5125bb8
Fernando de la Oliva <fernandoliva9@gmail.com>
2021-11-23 11:42:20 +0100

    modify cors

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require ('express')
 const mongoose = require ('mongoose')
 const cloudinary = require("cloudinary").v2;
 const morgan = require('morgan');
-// const cors = require("cors");
+const cors = require("cors");
 require('dotenv').config();
 
 const petsRoutes = require('./routes/petsRoutes')
