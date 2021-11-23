47774f52b6e4dd6244e999043bd0fce086daa9e1
Fernando de la Oliva <fernandoliva9@gmail.com>
2021-11-23 11:41:37 +0100

    modify cors

--- src/app.js
+++ src/app.js
@@ -18,18 +18,11 @@ cloudinary.config({
     api_secret: process.env.API_SECRET,
 });
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", '*');
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-  next();
-});
 
-// app.use(cors({
-// origin: ['*'],
-// credentials: true,
-// }));
+app.use(cors({
+origin: ['*'],
+credentials: true,
+}));
 
 //MIDDLEWARES
 app.use(morgan('dev'));
